f05a09fad8ca3af7503774eacc1a6cad9357ad24
Samuel Adebayo <sambayo28@gmail.com>
2022-02-13 10:19:59 +0100

    write minimum validation for users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :userName, presence: true, uniqueness: { case_sensitive: false };
+  validates :firstName, presence: true;
+  validates :firstName, presence: true;
+  validates :email, presence: true, uniqueness: { case_sensitive: false };
+  validates :password, presence: true;
 end
