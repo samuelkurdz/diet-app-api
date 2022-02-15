3f96ba7f66b444a9143ab23e999add83ac5efc58
Samuel Adebayo <sambayo28@gmail.com>
2022-02-15 21:12:00 +0100

    adding first test

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -3,5 +3,19 @@ module V1
     def index
       render json: User.all
     end
+
+    def create
+      user = User.new(user_params);
+      if user.save
+        render json: user, status: :created
+      else
+        render json: user.errors, status: :unprocessable_entity
+      end
+    end
+
+
+    def user_params
+      params.require(:user).permit(:userName, :firstName, :lastName, :email, :password);
+    end
   end  
 end
---
+++ spec/requests/countries_spec.rb
@@ -0,0 +1,14 @@
+
+require 'rails_helper'
+
+describe 'Countries API', type: :request do
+  it 'returns all countries' do
+    FactoryBot.create(:country, name: 'Yemen', alpha2_code: 'YE', alpha3_code: 'YEM')
+    FactoryBot.create(:country, name: 'Zambia', alpha2_code: 'ZM', alpha3_code: 'ZMB')
+    get '/v1/countries'
+
+    expect(response).to have_http_status(:success);
+    # puts JSON.parse(response.body);
+    expect(JSON.parse(response.body).size).to eq(2);
+  end
+end
